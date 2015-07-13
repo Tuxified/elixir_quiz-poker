3d6f7837686006da92e38c0309b6f125292983a0
Tonći Galić <tuxified@gmail.com>
2015-07-13 23:37:41 +0200

    Move attributes into functions, more flexible

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,14 +1,16 @@
 defmodule Cards do
-  @colours [:hearts, :clubs, :diamonds, :spades]
-  @numbers Enum.to_list(2..10) ++ [:jack, :queen, :king, :ace]
-  
+
+  @doc """
+  Creates new deck of shuffled cards
+  """
   def new_deck() do
-    :random.seed(:os.timestamp) # set seed for randomazation
-    for colour <- @colours, number <- @numbers do
+    :random.seed(:os.timestamp) # set seed for randomization
+    for colour <- colours, number <- numbers do
       [ colour, number ]
     end
     |> Enum.shuffle
   end
 
-  def numbers, do: @numbers
+  def colours, do: [:hearts, :clubs, :diamonds, :spades]
+  def numbers, do: Enum.to_list(2..10) ++ [:jack, :queen, :king, :ace]
 end
